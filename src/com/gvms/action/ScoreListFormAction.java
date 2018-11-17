d5f9f3774f6ef881b5f7db5c1d15c6b0eb78023c
dlgks0733 <dlgks0733@naver.com>
2018-11-17 21:01:02 +0900

    dds

--- src/com/gvms/action/ScoreListFormAction.java
+++ src/com/gvms/action/ScoreListFormAction.java
@@ -22,6 +22,7 @@ public class ScoreListFormAction implements Action{
 		
 //		StuListAction stuListAction = new StuListAction();
 //		stuListAction.execute(request, response);
+		System.out.println("dd");
 		
 		StudentDao stuDao = StudentDao.getInstance();
 		ArrayList<StudentVO> list = stuDao.StudentTotalList();
