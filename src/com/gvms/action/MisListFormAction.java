5096b3375662475286157245c8f3022b7f2ef724
dhtkdwnsdi <dhtkdwnsdi@gmail.com>
2018-11-17 19:42:10 +0900

    mis리스ㅡ 폼 액션

---
+++ src/com/gvms/action/MisListFormAction.java
@@ -0,0 +1,42 @@
+package com.gvms.action;
+
+/**
+ * 
+ * @author OSJ
+ *
+ */
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.gvms.dao.MisDAO;
+import com.gvms.vo.MisVO;
+
+public class MisListFormAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String url = "/mis/misList.jsp";
+		
+		
+		MisDAO mDao = MisDAO.getInstance();
+		
+		List<MisVO> misList = mDao.selectMis();
+		
+		request.setAttribute("misList", misList);
+		
+		System.out.println(misList);
+		
+		
+		
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+	}
+
+}
