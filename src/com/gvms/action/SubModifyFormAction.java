95db6bc57588b46fbb92fcbadff96ad48ce125f8
종욱 <@192.168.0.10>
2018-11-20 21:18:21 +0900

    submodifyformAction 추가

---
+++ src/com/gvms/action/SubModifyFormAction.java
@@ -0,0 +1,26 @@
+package com.gvms.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gvms.dao.SubjectDao;
+
+public class SubModifyFormAction {
+
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = "/subject/modify.jsp";
+		
+		SubjectDao sdao = SubjectDao.getInstance();
+		sdao.selectAllList(url);
+
+		
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+	}
+}
